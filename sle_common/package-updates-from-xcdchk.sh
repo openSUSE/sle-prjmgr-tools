7ed8290ec93ecdf0a2a919f3c399fea9c20329d8
Stefan Weiberg <sweiberg@suse.com>
2022-12-06 11:08:22 +0100

    improve output for direct usage:
    * list formatting of package results
    * adding queries for direct usage in JIRA

--- sle_common/package-updates-from-xcdchk.sh
+++ sle_common/package-updates-from-xcdchk.sh
@@ -18,21 +18,21 @@ ORIGIN_VERSION=${4:-SLE-15-SP5}
 
 
 echo "Updated packages: "
-grep "^o Update" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep "^$line\." $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-updated-RPMs; done | sed 's/\..*:/:/' | uniq
-grep "^o Added" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep "^$line\." $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-updated-RPMs; done | sed 's/\..*:/:/' | uniq
+grep "^o Update" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep "^$line\." $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-updated-RPMs; done | sed 's/\..*:/:/' | sed 's/^/\* /' | uniq
+grep "^o Added" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep "^$line\." $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-updated-RPMs; done | sed 's/\..*:/:/' | sed 's/^/\* /' | uniq
 
 echo
 echo "Added packages: "
-grep "^o Added" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep ^$line $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-new-RPMs; done | sed -e 's/\(.*\)-\([^-]*\)\-\([^-]*\)\..*/\1 \2-\3/' | uniq
-grep "^o Update" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep ^$line $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-new-RPMs; done | sed -e 's/\(.*\)-\([^-]*\)\-\([^-]*\)\..*/\1 \2-\3/' | uniq
+grep "^o Added" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep ^$line $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-new-RPMs; done | sed -e 's/\(.*\)-\([^-]*\)\-\([^-]*\)\..*/\1 \2-\3/' | sed 's/^/\* /' | uniq
+grep "^o Update" ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | awk '{print $3}' | while read -r line; do grep ^$line $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-new-RPMs; done | sed -e 's/\(.*\)-\([^-]*\)\-\([^-]*\)\..*/\1 \2-\3/' | sed 's/^/\* /' | uniq
 
 echo
 echo "Removed packages: "
-cat $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-missing-RPMs | grep 'x86_64\|noarch' | sed -e 's/-[^-]*\..*//' | egrep -v "^kernel|.*debugsource.*|.*debuginfo.*" | uniq
+cat $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-missing-RPMs | grep 'x86_64\|noarch' | sed -e 's/-[^-]*\..*//' | egrep -v "^kernel|.*debugsource.*|.*debuginfo.*" | sed 's/^/\* /' | uniq
 
 echo
 echo "Downgraded packages: "
-cat $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-downgraded-RPMs | sed 's/\..*:/:/' | uniq 
+cat $ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2-downgraded-RPMs | sed 's/\..*:/:/' | sed 's/^/\* /' | uniq 
 
 echo
 echo "Mentioned bug references: "
@@ -40,10 +40,62 @@ cat ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | while read -
 
 echo
 echo "Mentioned JIRA references: "
-cat ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | egrep -o 'jsc#SLE-[0-9]{5}|jsc#PED-[0-9]{1,5}' | sort | uniq
+cat ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | egrep -o 'jsc#SLE-[0-9]{5}|jsc#PED-[0-9]{1,5}' | sort | uniq | paste -s -d, -
+
+PED=$(cat ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | egrep -o 'PED-[0-9]{1,5}' | sort | uniq | paste -s -d, -)
 
 echo
-echo "Query for JIRA"
-echo "issue in (" 
-cat ChangeLog-$ORIGIN_VERSION-Full-Test-Build$BUILD1-Build$BUILD2 | egrep -o 'PED-[0-9]{1,5}' | sort | uniq | paste -s -d, - 
-echo ")"
+echo "JIRA query for incorrect state"
+echo "=============================="
+echo "Query:"
+echo "------"
+echo "issue in (${PED}) AND status NOT IN (\"QE Open\",\"QE In Progress\",\"QE Blocked\",\"Engineering Done\",\"Dev In Progress\",\"IBS Integration\") AND type = Implementation"
+echo "Comment:"
+echo "--------"
+echo "A submit request referencing this feature has been merged into build${BUILD2}."
+echo "Please update the state of this ticket, as it doesn't reflect the correct state of development."
+echo "Labels:"
+echo "-------"
+echo "Add status:wait_for_status"
+echo "Add status:code_merged"
+
+echo
+echo "JIRA query for still under development"
+echo "======================================"
+echo "Query:"
+echo "------"
+echo "issue in (${PED}) AND status = \"Dev In Progress\" AND type = Implementation"
+echo "Comment:"
+echo "--------"
+echo "A submit request referencing this feature has been merged into build${BUILD2}."
+echo "Labels:"
+echo "-------"
+echo "Remove status:wait_for_status"
+echo "Add status:code_merged"
+
+echo
+echo "JIRA query for already completed features"
+echo "========================================="
+echo "Query:"
+echo "------"
+echo "issue in (${PED}) AND status IN (\"QE Open\",\"QE In Progress\",\"QE Blocked\",\"Engineering Done\") AND type = Implementation"
+echo "Comment:"
+echo "--------"
+echo "A submit request referencing this feature has been merged into build${BUILD2}."
+echo "Labels:"
+echo "-------"
+echo "no handling required, only remove stale \"status:\" labels"
+
+echo
+echo "JIRA query for features ready to transition"
+echo "==========================================="
+echo "Query:"
+echo "------"
+echo "issue in (${PED}) AND status \"IBS Integration\" AND type = Implementation"
+echo "Comment:"
+echo "--------"
+echo "A submit request referencing this feature has been merged into build${BUILD2}."
+echo "Labels:"
+echo "-------"
+echo "Remove status:code_merged"
+echo "Remove status:wait_for_status"
